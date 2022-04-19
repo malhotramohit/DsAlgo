65b832fd17c53c478cd723cfed6c8338d44540fc
Mohit Malhotra <mohit.malhotra@nagarro.com>
2022-04-19 19:49:36 +0530

    first commit

---
+++ src/ds/string/Lengthoflongestsubstringwithoutrepeatingcharacters.java
@@ -0,0 +1,20 @@
+package ds.string;
+
+public class Lengthoflongestsubstringwithoutrepeatingcharacters {
+
+	public static void main(String[] args) {
+		/**
+		 * Given a string str, find the length of the longest substring without
+		 * repeating characters.
+		 * 
+		 * For “ABDEFGABEF”, the longest substring are “BDEFGA” and “DEFGAB”, with
+		 * length 6. For “BBBB” the longest substring is “B”, with length 1. For
+		 * “GEEKSFORGEEKS”, there are two longest substrings shown in the below
+		 * diagrams, with length 7
+		 */
+		
+		
+
+	}
+
+}
